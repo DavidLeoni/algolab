0d3343404c5563f24455b8e7d9df188e2f4c5aaa
David Leoni <davidleonigit-github@yahoo.com>
2016-11-22 11:59:50 +0100

    added custom.html

--- algolab.py
+++ algolab.py
@@ -1,6 +1,13 @@
-import sys    
+import sys
 import unittest
+from IPython.core.display import HTML
+
 
 def run(testcase):        
     suite = unittest.TestLoader().loadTestsFromTestCase(testcase)
     unittest.TextTestRunner(verbosity=1,stream=sys.stderr).run( suite )
+
+def init():
+    html = open("./custom.html", "r").read()
+    return HTML(html)
+
