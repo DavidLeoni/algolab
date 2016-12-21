0c7df63a7957952241d08bd24247f370000b7191
David Leoni <davidleonigit-github@yahoo.com>
2016-12-21 14:09:58 +0100

    update

--- exam-simulation/exam/exercise1.py
+++ exam-simulation/exam/exercise1.py
@@ -3,13 +3,7 @@ import unittest
 def insertion_sort(A):
     """ Sorts in-place list A with insertion sort.  """
                                          
-    for i in range(1, len(A)):                
-        temp = A[i]                       
-        j = i                             
-        while j > 1 and A[j-1] > temp:    
-            A[j] = A[j-1]                  
-            j -= 1                         
-        A[j] = temp                           
+    raise Exception("TODO implement me !")
     
 
 class InsertionSortTest(unittest.TestCase):
